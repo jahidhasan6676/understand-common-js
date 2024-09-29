55f7608360a2b9d418116f140a3ff6a2740af531
jahidhasan6676 <jh18186676@gmail.com>
2024-09-29 09:02:01 +0600

    added the common javascript

---
+++ advanced.js
@@ -0,0 +1,85 @@
+// class-6 > ----------------->
+
+// function kitchen(){
+//     let roast = 0;
+//     return function(){
+//         roast++;
+//         return roast;
+//     }
+// }
+// const firstServer = kitchen();
+// console.log(firstServer())
+// console.log(firstServer())
+// console.log(firstServer())
+// console.log(firstServer())
+// console.log(firstServer())
+
+
+// class-7 > callback ----------------------------------->
+
+// function greeting(greetingHandler, name, id){
+//     greetingHandler(name, id);
+// }
+// // // const number = 40;
+// // const object = {price: 40000, brad: 'lenovo', memory: '8gb'}
+// // greeting(object);
+
+// function greetingHandler(name){
+//     console.log('Good Morning', name)
+// }
+// function greetingEvening(name){
+//     console.log('Good Evening', name)
+// }
+// function goodName(id){
+//     console.log('id number 0', id)
+// }
+// greeting(greetingHandler, 'MD: Jahid Hasan')
+// greeting(greetingEvening, 'MD: kamrul Hasan')
+// greeting(goodName, 'my id number jani na')
+
+// class-8 > value pass ---------------------->
+
+// function sum(a, b, c){
+//     const args = [...arguments];
+//     // console.log(args);
+    
+//     const result = a + b + c;
+//     return result;
+// }
+// const total = sum(45, 56, 67, 78, 23, 12);
+// // console.log(total)
+
+// console.log(sum.length)
+
+
+// let num1 = 5;
+// let num2 = 7;
+
+// function multiply(a, b){
+//     a = 10;
+//     const result = a + b;
+//     return result;
+// }
+// console.log(num1)
+// const total = multiply(num1, num2);
+// // console.log(total)
+
+// let students1 = {name: 'jahid', partner: 'marjan'};
+// let students2 = {name: 'mehedi', partner: 'riya'};
+
+// function makeMovie(couple1, couple2){
+//     couple1.name = 'kamrul';
+//     couple2.partner = 'tanzina'
+// }
+// makeMovie(students1, students2);
+// console.log(students1, students2)
+
+// let rolllist1 = [12, 34, 45, 6, 67, 8, 43];
+// let rolllist2 = [13, 35, 68, 90, 10, 9, 4];
+
+// function list(list1, list2){
+//     list1[5] = 100;
+//     list2[3] = 400;
+// }
+// list(rolllist1, rolllist2);
+// console.log(rolllist1, rolllist2);
---
+++ common.js
@@ -0,0 +1,126 @@
+// class-1 > common js-------------------------->
+
+// let x = 5;
+// let y = x;
+// console.log(x, y);
+// y = 7;
+// console.log(x, y)
+
+// let p = {job: 'web developer'};
+// let q = p;
+// q.job = 'grapic design';
+// // q = {job: 'backend developer'}
+// console.log(p, q);
+
+
+// class-2 > null and undefined-------------->
+
+// let first;
+// console.log(first)
+
+// function second(a, b){
+//     const total = a + b;
+// }
+// const result = second(23, 45);
+// console.log(result)
+
+function third(a, b, c, d){
+    const total = a + b;
+   
+    
+}
+// third(2, 5)
+// function negative(a, b){
+//     if(a < 0 || b < 0){
+//         return
+//     }
+    
+//       return  a + b;
+    
+// }
+// const total = negative(5, -5);
+// console.log(total)
+
+// const fifth = {id: 2, age: 35, name:'jahid'};
+// console.log(fifth.pesa)
+
+// const six = [3, 45, 65, 78];
+// console.log(six[6])
+
+// const name = null;
+// console.log(name)
+
+
+// class-3 > truthy and falsy -------------------------->
+
+// const x = ' ';
+// if(x){
+//     console.log('value of x is truthy');
+
+// }
+// else{
+//     console.log('value of x is falsy')
+// }
+
+// const y = 0;
+// if(!y){
+//     console.log('value is falsy')
+// }
+// else{
+//     console.log('value is truthy')
+// }
+// const x = '0';
+// if(!!x){
+//     console.log('value is truthy')
+// }
+// else{
+//     console.log('value is falsy')
+// }
+
+
+// class-4 > == vs === difference --------------------->
+
+// const first = [];
+// const second = first;
+// if(first == second){
+//     console.log('value are equal')
+// }
+// else{
+//     console.log('value are not equal')
+// }
+
+// function add(a, b){
+//     const total = a + b;
+//     if(b < 5){
+//         const sum = 20 + b;
+        
+//     }
+//     else{
+//         var sum2 = 20 + b;
+//     }
+//     console.log(sum2)
+//     // console.log(sum)
+//     return total;
+// }
+// // console.log(total)
+// add(5, 7);
+// 
+// copy20();
+// copy10();
+// // hosting----------->
+// for(let i = 0; i < 5; i++){
+//     // console.log(i)
+// }
+// // console.log('outside', i)
+
+
+// function copy20(){
+//     console.log('inside prints: 20')
+// }
+
+// const add = function copy10(){
+//     console.log('inside prints: 40')
+// }
+
+
+
